0322591399c520f53c5504b7d5fd9737203d4353
catalinag12 <catalina.georgiana12@yahoo.com>
2020-10-20 16:43:27 +0300

    hw2

---
+++ app/src/main/java/com/example/restaurantsapplication/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.restaurantsapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.restaurantsapplication.restaurants.RestaurantsActivity;
+
+public class SplashActivity extends AppCompatActivity {
+   private static final int _splashTime=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+                Intent transitionIntent=new Intent(getApplicationContext(), RestaurantsActivity.class);
+                startActivity(transitionIntent);
+            }
+        }, _splashTime);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/restaurantsapplication/restaurants/RestaurantsActivity.java
@@ -0,0 +1,17 @@
+package com.example.restaurantsapplication.restaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.restaurantsapplication.R;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+
+    }
+}
