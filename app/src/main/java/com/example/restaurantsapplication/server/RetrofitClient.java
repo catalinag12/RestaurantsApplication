6fa8b9e4687d14aadc08d3650be2b4ac1caa799b
catalinag12 <catalina.georgiana12@yahoo.com>
2020-10-28 17:17:16 +0200

    added http logging intercepter

--- app/src/main/java/com/example/restaurantsapplication/server/RetrofitClient.java
+++ app/src/main/java/com/example/restaurantsapplication/server/RetrofitClient.java
@@ -1,4 +1,6 @@
 package com.example.restaurantsapplication.server;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,11 +9,13 @@ public class RetrofitClient {
     public static final String BASE_URL="https://04bc048c-2ea7-49a3-a2c0-4018e398e7e7.mock.pstmn.io/";
 
 
-
     public static Retrofit getInstance(){
+
+
          Retrofit  instance= new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(getOkHttpClient())
                     .build();
         return  instance;
 
@@ -22,4 +26,16 @@ public class RetrofitClient {
         return retrofitInterface;
     }
 
+    public static OkHttpClient getOkHttpClient() {
+        HttpLoggingInterceptor httpLoggingInterceptor=getHttpLoggingInterceptor();
+        return new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
+    }
+
+    private static HttpLoggingInterceptor getHttpLoggingInterceptor(){
+        HttpLoggingInterceptor httpLoggingInterceptor=new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return  httpLoggingInterceptor;
+
+    }
 }
